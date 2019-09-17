1a5ec3f7e159425bc9e76758016e9e3b97fcff57
Francisco Franco <fhfranco32@gmail.com>
2019-09-17 17:42:54 -0400

    deploying to heroku for testing

--- src/api/addUserToDatabase.js
+++ src/api/addUserToDatabase.js
@@ -11,6 +11,16 @@ var knex = require('knex')({
   }
 })
 
+// export default function addUser (name, email, password) {
+//   return knex('users').insert({
+//     name: name,
+//     email: email,
+//     password: password
+//   }).then().finally(function () {
+//     knex.destroy()
+//   })
+// }
+
 let name = 'Francisco Franco'
 let email = 'fhfranco32@gmail.com' // change to get from front end when user inputs email, from json?
 let password = 'hello' // encode and change
---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    port: '5437',
+    user: 'postgres',
+    password: '',
+    database: 'contact-manager-database'
+  }
+})
+
+module.exports = knex
