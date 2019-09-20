cf2ecad043bb0351b16f8eb71aba2eeb6099a8cf
Francisco Franco <fhfranco32@gmail.com>
2019-09-20 19:01:23 -0400

    routes file to define endpoints

---
+++ server/src/routes.js
@@ -0,0 +1,45 @@
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+
+module.exports = (app, knex) => {
+  app.post('/register', AuthenticationControllerPolicy.register, async (req, res) => {
+    await knex('users')
+      .insert({
+        name: 'test',
+        email: req.body.email,
+        password: req.body.password
+      })
+      .then(function (user) {
+        res.send({
+          message: `Hello ${req.body.email}, your user was registered!`
+        })
+      })
+      .catch(e => {
+        res.send({
+          error: 'This email is already in use.'
+        })
+      })
+  })
+
+  app.post('/login', async (req, res) => {
+    const {email, password} = req.body
+    const user = await knex.select().from('users')
+      .where({ email: email, password: password })
+      .then()
+      .catch(e => {
+        res.send({
+          error: 'Error when fetching user from database.'
+        })
+      })
+
+    if (user.length === 0) {
+      return res.send({
+        error: 'User not found.'
+      })
+    }
+
+    res.send({
+      message: `Hello ${req.body.email}, Welcome back.`,
+      user: user
+    })
+  })
+}
