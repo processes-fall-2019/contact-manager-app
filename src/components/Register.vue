5f95ce746056bdd5c74431f526308f42b6bb35bb
Daniel <dmorales9519@gmail.com>
2019-09-25 19:25:31 -0400

    front-end vuetify
    Used vuetify for the register page.

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,24 +1,57 @@
 <template>
-  <div>
-      <h1> Register </h1>
+  <v-parallax dark src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+    <v-row>
+      <v-flex>
+        <div class="fullScreen">
+          <v-toolbar flat dense class="black" dark>
+            <v-toolbar-title>Register</v-toolbar-title>
+          </v-toolbar>
+            
 
-      <input type="email" name="email" v-model="email" placeholder="email"/>
-      <input type="password" name="password" v-model="password" placeholder="password"/>
+              <!-- <input type="email" name="email" v-model="email" placeholder="email"/>
+              <br>
+              <input type="password" name="password" v-model="password" placeholder="password"/> -->
 
-      <br>
+                 <v-col cols="12" sm="6" md="3">
+                  <v-text-field
+                    label="email"
+                    filled
+                    type="email"
+                    name="email"
+                    v-model="email"
+                    placeholder=""
+                  ></v-text-field>
+                </v-col>
 
-      <div class="error" v-html="error"/>
+                 <v-col cols="12" sm="6" md="3">
+                  <v-text-field
+                    label="password"
+                    filled
+                    type="password"
+                    name="password"
+                    v-model="password"
+                    placeholder=""
+                  ></v-text-field>
+                </v-col>
 
-      <br>
-      <router-link :to="{name: 'HelloWorld'}">
-        <button @click="register"> Register </button>
-      </router-link>
+              <br>
 
-      <router-link :to="{name: 'HelloWorld'}">
-        <button> Cancel </button>
-      </router-link>
+              <div class="error" v-html="error"/>
 
-  </div>
+              <br>
+              <router-link :to="{name: 'HelloWorld'}">
+                <v-btn class="cyan" @click="register"> Register </v-btn>
+              </router-link>
+              <router-link :to="{name: 'HelloWorld'}">
+                <v-btn class="cyan"> Cancel </v-btn>
+              </router-link>
+
+            
+        </div>
+      </v-flex>
+    </v-row>
+  </v-parallax>
+
 </template>
 
 <script>
@@ -54,5 +87,13 @@ export default {
 <style scoped>
   .error {
     color: red;
+  }
+
+  h1{
+    font-color: blue;
+  }
+
+  .email{
+    font-color:blue;
   }
 </style>
