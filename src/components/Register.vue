8c70976e2be2dbaa01cf826af748b6fd91ed3492
Francisco Franco <fhfranco32@gmail.com>
2019-09-26 21:15:47 -0400

    updated register page

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -6,7 +6,7 @@
             <v-toolbar-title class="center">Registration</v-toolbar-title>
           </v-toolbar>
                <v-row align="center" justify="center">
-                <div class="center"> 
+                <div class="center">
                   <v-col>
                     <v-text-field
                       label="email"
@@ -17,11 +17,10 @@
                       placeholder="">
                     </v-text-field>
                   </v-col>
-
                   <v-col>
-                    <v-text-field label="password" 
-                      filled 
-                      type="password" 
+                    <v-text-field label="password"
+                      filled
+                      type="password"
                       name="password"
                       v-model="password"
                       placeholder="">
@@ -32,15 +31,35 @@
             <br>
             <div class="error" v-html="error"/>
             <br>
-            <router-link :to="{name: 'HelloWorld'}">
+            <!-- <router-link :to="{name: 'HelloWorld'}"> -->
               <v-btn dark class="cyan" @click="register"> Register </v-btn>
-            </router-link>
+            <!-- </router-link> -->
             <router-link :to="{name: 'HelloWorld'}">
               <v-btn dark class="cyan"> Cancel </v-btn>
             </router-link>
         </div>
       </v-flex>
   </v-parallax>
+<!-- =======
+      <input type="email" name="email" v-model="email" placeholder="email"/>
+      <input type="password" name="password" v-model="password" placeholder="password"/>
+
+      <br>
+
+      <div class="error" v-html="error"/>
+
+      <br>
+      <! <router-link :to="{name: 'HelloWorld'}">
+        <button @click="register"> Register </button>
+      </router-link> -->
+      <!-- <button @click="register"> Register </button>
+
+      <router-link :to="{name: 'HelloWorld'}">
+        <button> Cancel </button>
+      </router-link> -->
+
+  <!-- </div> -->
+<!-- >>>>>>> Stashed changes -->
 </template>
 
 <script>
@@ -63,6 +82,17 @@ export default {
           password: this.password
         })
 
+        if (response.data.error) {
+          alert(response.data.error)
+          return false
+        }
+
+        this.$router.push({
+          name: 'HelloWorld'
+        })
+
+        alert('Hello ' + this.email + ' your user was successfully registered!')
+
         console.log(response.data)
       } catch (error) {
         this.error = error.response.data.error
@@ -75,7 +105,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .error {
-    color: red;
+    color: black;
   }
 
   h1{
