fb3c3d6a5c0574a73a693f3302a94473d00d5b6f
Francisco Franco <fhfranco32@gmail.com>
2019-09-27 02:36:05 -0400

    created Contact component

---
+++ src/components/Contact.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>contact</h1>
+    <div v-for="contact in contacts" v-bind:key="contact.id">
+       <li>
+         {{ contact.first_name }}
+       </li>
+    </div>
+    <button> Add </button>
+    <button> Delete </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['contact-id'],
+
+  data () {
+    return {
+      contacts: this.$store.state.contacts
+    }
+  },
+
+  methods: {
+  }
+}
+
+</script>
+
+<style>
+</style>
