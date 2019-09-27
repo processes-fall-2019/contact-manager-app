646aa2e8ecba9eaf0f08b54b56ed3218b97e4e0c
Francisco Franco <fhfranco32@gmail.com>
2019-09-26 20:09:15 -0400

    updated contacts page

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -6,34 +6,86 @@
     <br>
     <button> Add </button>
     <button> Delete </button>
+    <br>
+    <br>
+    <button @click="getContacts"> List Contacts </button>
     &nbsp; &nbsp;
     <input type="search" name="searchRes" v-model="searchRes" placeholder="search..."/>
     <button> Search </button>
     <!-- TOdo: make delete and add buttons dynamic. (one delete button per contact) (an add button soemwhere) -->
+    <!-- TOdo: make a way to edit contacts -->
+    <!-- TODo: make contact its own component, then also create a contact list component -->
     <br>
+    <br>
+    <div>
+      <h3>List</h3>
+
+      <!-- <ul>
+        <li v-for="contact in contacts">
+          {{ contact }}
+        </li>
+      </ul> -->
+      <div v-for="contact in contacts">
+        <li>
+          {{ contact }}
+        </li>
+        </li>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import AuthenticationService from '../services/AuthenticationService'
+import ContactList from './ContactList'
+
 export default {
+  components: {
+    ContactList
+  },
   data () {
     return {
       email: '',
+      userId: this.$store.state.user[0].id,
       password: '',
       error: null,
       canLoginIn: false,
       searchRes: '',
-      contacts: ['franco', 'yo']
+      contacts: []
     }
   },
   methods: {
-    search () {
+    searchContacts () {
     },
     logout () {
       this.$store.dispatch('setUser', null)
       this.$router.push({
         name: 'HelloWorld'
       })
+    },
+    getUserId () {
+      return this.$store.state.user[0].id
+      // console.log(this.$store.state.user[0].id) // use to somehow get user id
+    },
+    async getContacts () {
+      try {
+        const response = await AuthenticationService.contacts({
+          user_id: this.userId
+        })
+
+        console.log('the ressy', response)
+        console.log('the res', response.data)
+        console.log('the res 2', this.userId)
+
+        let conts = response.data.filter(x => x.email)
+
+        this.contacts = conts
+
+        console.log('contacts', this.contacts)
+        return response
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
