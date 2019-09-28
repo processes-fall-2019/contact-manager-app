a4a362ab67b87f9081d24b44b9ceebf84bb73d9b
Francisco Franco <fhfranco32@gmail.com>
2019-09-28 17:46:45 -0400

    deploy to heroku for testing

--- knexfile.js
+++ knexfile.js
@@ -33,20 +33,15 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      host: '127.0.0.1',
-      port: '5437',
-      user: 'postgres',
-      password: '',
-      database: 'contact-manager-database'
-    },
+    connection: 'postgres://ipegmfaesrtcna:128e2a8ba320ada09c23c83b24cbb9961d10d087d0d49266ac41e4cce8ad0a22@ec2-54-235-180-123.compute-1.amazonaws.com:5432/d49ogkgahtr92q',
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
       tableName: 'knex_migrations'
-    }
+    ,
+    ssl: true
   }
 
 };
--- server/src/app.js
+++ server/src/app.js
@@ -11,15 +11,24 @@ app.use(bodyParser.json())
 app.use(cors())
 
 // establish connection with database using Knex ORM
+// var knex = require('knex')({
+//   client: 'pg',
+//   connection: {
+//     host: '127.0.0.1',
+//     port: '5437',
+//     user: 'postgres',
+//     password: '',
+//     database: 'contact-manager-database'
+//   }
+// })
+
 var knex = require('knex')({
   client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    port: '5437',
-    user: 'postgres',
-    password: '',
-    database: 'contact-manager-database'
-  }
+  connection: 'postgres://ipegmfaesrtcna:128e2a8ba320ada09c23c83b24cbb9961d10d087d0d49266ac41e4cce8ad0a22@ec2-54-235-180-123.compute-1.amazonaws.com:5432/d49ogkgahtr92q',
+  migrations: {
+    tableName: 'knex_migrations'
+  },
+  ssl: true
 })
 
 require('./routes')(app, knex)
