6802f2667d9ef6ab26593af98f979568129ff124
Francisco Franco <fhfranco32@gmail.com>
2019-09-28 17:55:49 -0400

    heroku changes

--- knexfile.js
+++ knexfile.js
@@ -32,16 +32,16 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
-    connection: 'postgres://ipegmfaesrtcna:128e2a8ba320ada09c23c83b24cbb9961d10d087d0d49266ac41e4cce8ad0a22@ec2-54-235-180-123.compute-1.amazonaws.com:5432/d49ogkgahtr92q',
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    ,
-    ssl: true
+      client: 'pg',
+      connection: 'postgres://ipegmfaesrtcna:128e2a8ba320ada09c23c83b24cbb9961d10d087d0d49266ac41e4cce8ad0a22@ec2-54-235-180-123.compute-1.amazonaws.com:5432/d49ogkgahtr92q',
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        tableName: 'knex_migrations'
+      ,
+      ssl: true
+    }
   }
-
 }
