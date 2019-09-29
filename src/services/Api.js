7aad904c608e12a3a0fcb7c9cc61eed910334ac6
Francisco Franco <fhfranco32@gmail.com>
2019-09-29 13:50:56 -0400

    heroku changes

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 export default () => {
   return axios.create({
     // baseURL: `http://localhost:8081`
-    baseURL: `https://contact-manager-prod.herokuapp.com`
+    // baseURL: `https://contact-manager-prod.herokuapp.com`
+    baseURL: ''
   })
 }
