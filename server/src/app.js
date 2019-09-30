93be6bd7c2f490302d685596cf1e2b11f1ebe9bf
Francisco Franco <fhfranco32@gmail.com>
2019-09-30 16:17:27 -0400

    heroku changes

--- server/src/app.js
+++ server/src/app.js
@@ -7,32 +7,33 @@ const app = express()
 const port = process.env.PORT || 8081
 var path = require('path')
 var serveStatic = require('serve-static')
+require('dotenv').config() // skkrrrr
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-// establish connection with database using Knex ORM
-var knex = require('knex')({
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    port: '5437',
-    user: 'postgres',
-    password: '',
-    database: 'contact-manager-database'
-  }
-})
-
-// // for production database
+// // establish connection with database using Knex ORM
 // var knex = require('knex')({
 //   client: 'pg',
-//   connection: 'postgres://ethoesmmnzcunu:901dc9decc44710ccf9b2a38b3c8562efb358f46ddb9ada48aa0492da190dc8c@ec2-54-235-180-123.compute-1.amazonaws.com:5432/dde8k4u6i7hq2m',
-//   migrations: {
-//     tableName: 'knex_migrations'
-//   },
-//   ssl: true
+//   connection: {
+//     host: '127.0.0.1',
+//     port: '5437',
+//     user: 'postgres',
+//     password: '',
+//     database: 'contact-manager-database'
+//   }
 // })
+
+// for production database
+var knex = require('knex')({
+  client: 'pg',
+  connection: 'postgres://ethoesmmnzcunu:901dc9decc44710ccf9b2a38b3c8562efb358f46ddb9ada48aa0492da190dc8c@ec2-54-235-180-123.compute-1.amazonaws.com:5432/dde8k4u6i7hq2m',
+  migrations: {
+    tableName: 'knex_migrations'
+  },
+  ssl: true
+})
 
 // app.get('/', function (req, res) {
 //   res.redirect('/HelloWorld')
@@ -79,7 +80,7 @@ var knex = require('knex')({
 
 require('./routes')(app, knex)
 
-app.use(express.static('client/build')) // del
+app.use(express.static(path.join(__dirname, 'client', 'build'))) // del
 app.get('*', function (req, res) { // del
   // res.sendFile(path.join(__dirname + '/dist/index.html'))
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
